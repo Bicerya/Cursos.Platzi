cda5a89faf70a35c57ab7d20c865c643b1f21283
Ernesto Lechuga <bicerya2023@gmail.com>
2023-01-22 21:28:56 -0600

    terminado de codigo JS

--- basicoProgramacion/nuevoCurso/mokepon/js/mokepon.js
+++ basicoProgramacion/nuevoCurso/mokepon/js/mokepon.js
@@ -1,27 +1,41 @@
 window.addEventListener('load', iniciarJuego)
 
-let ataqueJugador = 0
-let ataqueEnemigo = 0
-
+let ataqueJugador
+let ataqueEnemigo
+let vidasJugador = 3
+let vidasEnemigo = 3
 
 function iniciarJuego()
 {
+    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+    sectionSeleccionarAtaque.style.display = 'none'
+
     let botonMascotaJugador = document.getElementById('boton-mascotas')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 
     let botonFuego = document.getElementById('boton-fuego')
     botonFuego.addEventListener('click', ataqueFuego)
-
     let botonAgua = document.getElementById('boton-agua')
     botonAgua.addEventListener('click', ataqueAgua)
-
     let botonTierra = document.getElementById('boton-tierra')
     botonTierra.addEventListener('click', ataqueTierra)
+
+    let botonReiniciar = document.getElementById('boton-reiniciar')
+    botonReiniciar.addEventListener('click', reiniciarJuego)
+
+    botonReiniciar.style.display = 'none'
 }
 
 
 function seleccionarMascotaJugador()
 {
+
+    let sectionSeleccionarMascota = document.getElementById('seleccionar-mascota')
+    sectionSeleccionarMascota.style.display = 'none'
+
+    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+    sectionSeleccionarAtaque.style.display = 'block'
+
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya')
@@ -141,14 +155,17 @@ function crearMensaje(resultado)
     let sectionMensajes = document.getElementById('mensajes')
 
     let parrafo = document.createElement('p')
-    parrafo.innerHTML = 'Tu mascota ataco con ' + ataqueJugador + ', la mascota del enemigo ataco con ' + ataqueEnemigo + ' - ' +  resultado
+    parrafo.innerHTML = `Tu mascota ataco con ${ataqueJugador} la mascota del enemigo ataco con ${ataqueEnemigo} ${resultado}`
 
-    sectionMensajes.appendChild(parrafo)
+    sectionMensajes.appendChild(parrafo)    
 }
 
 
 function combate()
 {
+    let spanVidasJugador = document.getElementById('vidas-jugador')
+    let spanVidasEnemigo = document.getElementById('vidas-enemigo')
+
     if(ataqueEnemigo == ataqueJugador)
     {
         crearMensaje('Empate');
@@ -156,9 +173,56 @@ function combate()
     else if ((ataqueJugador == 'Fuego' && ataqueEnemigo == 'Tierra') || (ataqueJugador == 'Agua' && ataqueEnemigo == 'Fuego') || (ataqueJugador == 'Tierra' && ataqueEnemigo == 'Agua'))
     {
         crearMensaje('Ganaste!');
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
     } 
     else
     {
         crearMensaje('Perdiste 😢');
+        vidasJugador--
+        spanVidasJugador.innerHTML = vidasJugador
     }
+
+    revisarVidas()
+
+}
+
+function revisarVidas()
+{
+    if(vidasEnemigo == 0)
+    {
+        crearMensajeFinal(`Felicitaciones Ganaste!!`)
+    }
+    else if (vidasJugador == 0)
+    {
+      crearMensajeFinal(`Lo siento, Perdiste 🥲`)
+    }
+
+}
+
+function crearMensajeFinal(resultadoFinal)
+{
+    let sectionMensajes = document.getElementById('mensajes')
+
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = resultadoFinal
+
+    sectionMensajes.appendChild(parrafo)   
+    
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.disabled = true
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.disabled = true
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.disabled = true
+
+    let botonReiniciar = document.getElementById('boton-reiniciar')
+    botonReiniciar.addEventListener('click', reiniciarJuego)
+
+    botonReiniciar.style.display = 'block'
+}
+
+function reiniciarJuego()
+{
+    location.reload()
 }
